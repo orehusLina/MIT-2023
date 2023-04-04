e243729ec7b7e833cdbd53e2c97c9de624da58a4
Alina Orekhova <orehus.lina@mail.ru>
2023-04-04 19:12:02 +0400

    Added recursive Fibbonacci function

--- secondTask.cpp
+++ secondTask.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int fib(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    else return fib(n-1) + fib (n-2);
+}
+
 int main() {
-    cout << "Hello, World!" << '\n';
+    cout << "fib(10) = " << fib(10) << '\n';
     return 0;
 }
